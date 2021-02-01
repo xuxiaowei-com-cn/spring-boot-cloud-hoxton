2ae18e47effaeb3908437b5e7d35b687400d483e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-01 16:52:23 +0800

    资源服务器 配置

---
+++ oauth2/src/main/java/cn/com/xuxiaowei/cloud/oauth2/configuration/ResourceServerConfigurerAdapterConfiguration.java
@@ -0,0 +1,35 @@
+package cn.com.xuxiaowei.cloud.oauth2.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * 资源服务器 配置
+ *
+ * @author xuxiaowei
+ * @see <a href="http://127.0.0.1:10601/sns/userinfo?access_token=">访问资源</a>
+ * @see <a href="http://localhost:10601/sns/userinfo?access_token=">访问资源</a>
+ * @since 0.0.1
+ */
+@Configuration
+public class ResourceServerConfigurerAdapterConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // 社交资源
+        http.antMatcher("/sns/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/sns/userinfo")
+                .access("#oauth2.hasAnyScope('snsapi_base','snsapi_userinfo')")
+                .antMatchers(HttpMethod.POST, "/sns/userinfo")
+                .access("#oauth2.hasAnyScope('snsapi_base','snsapi_userinfo')");
+
+        // 需要放在最后
+        super.configure(http);
+
+    }
+
+}
