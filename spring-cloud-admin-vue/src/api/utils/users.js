a4256899254a575544056ef692061856d31f48f0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-04 00:23:52 +0800

    是否在线

---
+++ spring-cloud-admin-vue/src/api/utils/users.js
@@ -0,0 +1,14 @@
+import request from '../../utils/request'
+
+/**
+ * 是否在线
+ * @param params 参数
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export function onLineWhether(params) {
+    return request({
+        url: 'http://localhost:10401/on-line/whether',
+        method: 'post',
+        params: params
+    })
+}
