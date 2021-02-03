3dcbb42fb25627fb82761d32ee4535d7b44e4e90
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-04 00:45:52 +0800

    路由鉴权并跳转

--- spring-cloud-admin-vue/src/router/index.js
+++ spring-cloud-admin-vue/src/router/index.js
@@ -13,32 +13,30 @@ const login = ref('/login')
 
 router.beforeEach((to, from, next) => {
 
-    if (login.value === to.path) {
-        // 登录页面，无需进行鉴权
-        next()
-    } else {
-        onLineWhether().then(response => {
-            const data = response.data
-            if (data != null) {
-                if (data.whether === true) {
-                    // 已登录，无需操作
-                    next()
-                } else {
-                    // 未登录，强制跳转到登录页面
-                    next({
-                        path: login.value,
-                        query: {redirect: to.fullPath} // 将要跳转路由的path作为参数，传递到登录页面
-                    })
-                }
-            } else {
-                // 鉴权时响应数据异常，强制跳转到登录页面
+    onLineWhether().then(response => {
+        const data = response.data
+        const whether = data != null && data.whether === true; // 是否登录
+
+        if (whether) { // 已登录
+            if (login.value === to.path) { // 登录页面
+                // 强制跳转到登录首页
+                location.hash = '/'
+            } else { // 非登录页面
+                next()
+            }
+        } else { // 未登录
+            if (login.value === to.path) { // 登录页面
+                next()
+            } else { // 非登录页面
+                // 强制跳转到登录页面
                 next({
                     path: login.value,
                     query: {redirect: to.fullPath} // 将要跳转路由的path作为参数，传递到登录页面
                 })
             }
-        })
-    }
+        }
+
+    })
 
 })
 
