285c2d3f51a9df1c2d899d9dbe20e87323eff167
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-20 22:08:12 +0800

    从 eureka 切换至 nacos

--- passport/src/main/java/cn/com/xuxiaowei/cloud/passport/PassportApplication.java
+++ passport/src/main/java/cn/com/xuxiaowei/cloud/passport/PassportApplication.java
@@ -3,9 +3,10 @@ package cn.com.xuxiaowei.cloud.passport;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationProperties;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -13,6 +14,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 登录 程序执行入口
+ * <p>
+ * Spring Cloud 使用注解 {@link EnableEurekaClient} 的方式提供 Eureka 客户端（禁用 Eureka）
+ * Spring Cloud 使用注解 {@link EnableConfigurationProperties#value()} 设置 {@link AutoServiceRegistrationProperties} 的方式提供 Nacos 客户端
  *
  * @author xuxiaowei
  * @since 0.0.1
@@ -21,11 +25,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableRedisHttpSession
 @EnableWebSecurity
 @EnableResourceServer
-@EnableEurekaClient
 @SpringBootApplication
 @EnableTransactionManagement
 @ServletComponentScan("cn.com.xuxiaowei.cloud.passport.servlet")
 @MapperScan("cn.com.xuxiaowei.cloud.passport.*.mapper.**")
+@EnableConfigurationProperties(AutoServiceRegistrationProperties.class)
 public class PassportApplication {
 
     public static void main(String[] args) {
