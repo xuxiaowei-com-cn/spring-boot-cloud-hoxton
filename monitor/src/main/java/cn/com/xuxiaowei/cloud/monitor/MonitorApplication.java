68e9f3a5b3713c1f8608bf907bde29b89e2dfd30
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-20 20:49:28 +0800

    从 eureka 切换至 nacos

--- monitor/src/main/java/cn/com/xuxiaowei/cloud/monitor/MonitorApplication.java
+++ monitor/src/main/java/cn/com/xuxiaowei/cloud/monitor/MonitorApplication.java
@@ -2,20 +2,23 @@ package cn.com.xuxiaowei.cloud.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationProperties;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * 断路器监控 程序执行入口
+ * <p>
+ * Spring Cloud 使用注解 {@link EnableEurekaClient} 的方式提供 Eureka 客户端（禁用 Eureka）
  *
  * @author xuxiaowei
  * @since 0.0.1
  */
 @EnableTurbine
-@EnableEurekaClient
 @EnableHystrixDashboard
 @SpringBootApplication
+@EnableConfigurationProperties(AutoServiceRegistrationProperties.class)
 public class MonitorApplication {
 
     public static void main(String[] args) {
