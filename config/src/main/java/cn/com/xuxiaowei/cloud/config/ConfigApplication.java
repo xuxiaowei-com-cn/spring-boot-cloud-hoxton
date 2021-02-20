72ba3f7c11000451df29895098c87b5db35ecb6f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-20 20:41:37 +0800

    禁用 Eureka

--- config/src/main/java/cn/com/xuxiaowei/cloud/config/ConfigApplication.java
+++ config/src/main/java/cn/com/xuxiaowei/cloud/config/ConfigApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.config.server.encryption.EncryptionController;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 配置 程序执行入口
  * <p>
- * Spring Cloud 使用注解 {@link EnableEurekaClient} 的方式提供 Eureka 客户端
+ * Spring Cloud 使用注解 {@link EnableEurekaClient} 的方式提供 Eureka 客户端（禁用 Eureka）
  * Spring Cloud 使用注解 {@link EnableConfigServer} 的方式提供 开启配置服务器
  * Spring Cloud 使用注解 {@link EnableConfigurationProperties#value()} 设置 {@link AutoServiceRegistrationProperties} 的方式提供 Nacos 客户端
  *
@@ -19,7 +18,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @see EncryptionController
  * @since 0.0.1
  */
-@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 @EnableConfigurationProperties(AutoServiceRegistrationProperties.class)
