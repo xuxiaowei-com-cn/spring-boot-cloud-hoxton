c1641e05780207a4b9cdd543e228aef49257fd28
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-21 20:10:16 +0800

    注释、异常

--- ui/src/main/java/cn/com/xuxiaowei/cloud/ui/test/hystrix/TestIHystrixService.java
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/test/hystrix/TestIHystrixService.java
@@ -1,6 +1,7 @@
 package cn.com.xuxiaowei.cloud.ui.test.hystrix;
 
 import cn.com.xuxiaowei.cloud.ui.test.entity.TestPassportI;
+import cn.com.xuxiaowei.cloud.ui.test.exception.TestIException;
 import cn.com.xuxiaowei.cloud.ui.test.exception.TestPassportException;
 import cn.com.xuxiaowei.cloud.ui.test.feign.TestIService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 /**
- * 测试 登录模块 Hystrix
+ * 测试 用户模块 Hystrix
  *
  * @author xuxiaowei
  * @since 0.0.1
@@ -29,10 +30,10 @@ public class TestIHystrixService {
     }
 
     /**
-     * 测试 登录模块 参数接收、保存数据 服务实现
+     * 测试 用户模块 参数接收、保存数据 服务实现
      *
-     * @param testPassportI 登录模块测试表，必填，否则调用失败
-     * @return 返回 测试 登录模块 结果
+     * @param testPassportI 用户模块测试表，必填，否则调用失败
+     * @return 返回 测试 用户模块 结果
      */
     @HystrixCommand(fallbackMethod = "saveFallback")
     public Map<String, Object> save(TestPassportI testPassportI) {
@@ -40,14 +41,14 @@ public class TestIHystrixService {
     }
 
     /**
-     * 测试 登录模块 参数接收、保存数据 异常数据
+     * 测试 用户模块 参数接收、保存数据 异常数据
      *
-     * @param testPassportI 登录模块测试表
+     * @param testPassportI 用户模块测试表
      * @return 返回 异常 结果
-     * @throws TestPassportException 测试登录模块 异常
+     * @throws TestIException 测试用户模块 异常
      */
-    public Map<String, Object> saveFallback(TestPassportI testPassportI) throws TestPassportException {
-        throw new TestPassportException("A0001", "用户模块故障");
+    public Map<String, Object> saveFallback(TestPassportI testPassportI) throws TestIException {
+        throw new TestIException("A0002", "用户模块故障");
     }
 
 }
