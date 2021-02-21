e24a2ba377a3c86ed98645599543dcb071ccaaa1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-21 16:38:09 +0800

    灰度 请求拦截器 {@link FeignClient}

---
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/ribbon/FeignGrayRequestInterceptor.java
@@ -0,0 +1,35 @@
+package cn.com.xuxiaowei.cloud.ui.ribbon;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * 灰度 请求拦截器 {@link FeignClient}
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Configuration
+public class FeignGrayRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+        Map<String, Collection<String>> headers = template.headers();
+        String gray = "gray";
+        if (headers.containsKey(gray)) {
+            Collection<String> grayValues = headers.get(gray);
+            Iterator<String> grayValuesIterator = grayValues.iterator();
+            String grayValue = grayValuesIterator.next();
+            if (Boolean.TRUE.toString().equals(grayValue)) {
+                RibbonRequestContextHolder.getCurrentContext().put(gray, Boolean.TRUE.toString());
+            }
+        }
+    }
+
+}
