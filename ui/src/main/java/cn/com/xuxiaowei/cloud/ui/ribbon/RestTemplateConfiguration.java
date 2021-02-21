32d9b45dcac0957d74dd152780c7be9fabacdea9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-21 16:18:35 +0800

    配置 负载均衡 {@link RestTemplate}

---
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/ribbon/RestTemplateConfiguration.java
@@ -0,0 +1,30 @@
+package cn.com.xuxiaowei.cloud.ui.ribbon;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * 配置 负载均衡 {@link RestTemplate}
+ *
+ * @author xuxiaowei
+ * @see LoadBalanced 负载均衡
+ * @since 0.0.1
+ */
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+        interceptors.add(new GrayInterceptor());
+        return restTemplate;
+    }
+
+}
