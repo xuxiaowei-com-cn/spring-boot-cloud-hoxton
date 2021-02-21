37f43db5a00a561a906a8b166c81d4c81544b350
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-21 14:04:22 +0800

    Scan

--- ui/src/main/java/cn/com/xuxiaowei/cloud/ui/UiApplication.java
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/UiApplication.java
@@ -1,8 +1,10 @@
 package cn.com.xuxiaowei.cloud.ui;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
