b1510794165b2cb9cdbff2978ecbb2399626d855
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-02-21 14:35:44 +0800

    灰度发布

---
+++ passport/src/main/java/cn/com/xuxiaowei/cloud/passport/test/controller/TestRestController.java
@@ -0,0 +1,35 @@
+package cn.com.xuxiaowei.cloud.passport.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 测试
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+    /**
+     * 灰度发布
+     *
+     * @param request  请求
+     * @param response 响应
+     * @return 返回 灰度发布
+     */
+    @RequestMapping("/echo")
+    public String echo(HttpServletRequest request, HttpServletResponse response) {
+        String gray = request.getHeader("Gray");
+        return String.format("LocalAddr：%s，LocalPort：%s，Header Gray：%s",
+                request.getLocalAddr(), request.getLocalPort(), gray);
+    }
+
+}
